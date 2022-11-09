99bca67d3cf971e8e2a621a6de4943236f28bccf
AzureAD\LeHuuVinh <vinhphac2@gmail.com>
2022-11-09 17:47:55 +0700

    fix path home page

--- src/Component/TicTacToe/index.js
+++ src/Component/TicTacToe/index.js
@@ -15,14 +15,14 @@ function TicTacToe(){
             <div className='menu-page'>
                 <nav className="game-nav">
                     <ul className="game-nav-list">
-                        <li className="game-nav-item"><NavLink to='/' className={navLinkClass}>Home</NavLink></li>
+                        <li className="game-nav-item"><NavLink to='/Game-Tic-Toc-Toe' className={navLinkClass}>Home</NavLink></li>
                         <li className="game-nav-item"><NavLink to='/tutorial' className={navLinkClass}>Tutorial</NavLink></li>
                         <li className="game-nav-item"><NavLink to='/contact' className={navLinkClass}>Contact</NavLink></li>
                     </ul>            
                 </nav>         
             </div>
             <Routes>
-                <Route path="/" element={ <Home/> } />
+                <Route path="/Game-Tic-Toc-Toe" element={ <Home/> } />
                 <Route path="tutorial" element={ <Tutorial/> } />
                 <Route path="contact" element={ <Contact/> } />
             </Routes>
